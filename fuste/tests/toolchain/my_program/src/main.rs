56faf380578e6862192a57324b147d0a45d01881
Liam Makena Monninger <liam@ramate.io>
2025-10-23 16:00:39 +0100

    debug: invalid instruction.

--- fuste/tests/toolchain/my_program/src/main.rs
+++ fuste/tests/toolchain/my_program/src/main.rs
@@ -6,7 +6,13 @@ use core::panic::PanicInfo;
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
 	// Replace with your entrypoint logic
-	loop {}
+	let mut j = 0;
+	for i in 0..10 {
+		assert_eq!(i, i);
+		j += i;
+	}
+
+	panic!("program finished");
 }
 
 #[panic_handler]
