9f9d8838315e08d5a0130463d3539e3164ccfc61
Liam Makena Monninger <liam@ramate.io>
2025-10-24 13:29:35 +0100

    debug: terminating main fails.

--- fuste/tests/toolchain/my_program/src/main.rs
+++ fuste/tests/toolchain/my_program/src/main.rs
@@ -4,29 +4,36 @@
 use core::arch::asm;
 use core::panic::PanicInfo;
 
-extern "C" {
-	static _stack_end: u32;
-}
-
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+fn _start() -> ! {
 	unsafe {
 		// set stack pointer
 		asm!("la sp, _stack_end", options(nomem, nostack, preserves_flags));
 	}
 	main();
+	exit()
+}
+
+extern "C" {
+	static _stack_end: u32;
 }
 
 #[no_mangle]
-pub extern "C" fn main() -> ! {
+pub extern "C" fn exit() -> ! {
+	unsafe {
+		asm!("ebreak", options(nomem, nostack, preserves_flags));
+	}
+	loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn main() {
 	// Replace with your entrypoint logic
 	let mut j = 0;
 	for i in 0..10 {
 		assert_eq!(i, i);
 		j += i;
 	}
-
-	panic!("program finished");
 }
 
 #[panic_handler]
