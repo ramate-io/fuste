d08edfaef8a497f2c6fbee9cc1271a7076e9ea62
Liam Makena Monninger <liam@ramate.io>
2025-10-26 18:01:38 +0000

    feat: simple macro rule for the program setup; we will likely replace this with a decarator soon.

--- fuste/tests/toolchain/my-fuste-program/src/main.rs
+++ fuste/tests/toolchain/my-fuste-program/src/main.rs
@@ -1,51 +1,22 @@
 #![no_std]
 #![no_main]
-
-use core::arch::asm;
-use core::panic::PanicInfo;
-use fuste::{exit, println, ExitStatus};
-
-#[no_mangle]
-fn _start() -> ! {
-	unsafe {
-		// set stack pointer
-		asm!(
-			"la sp, {stack}",
-			stack = sym _stack_end
-		);
-	}
-	_main();
-}
-
-extern "C" {
-	static _stack_end: u32;
-}
-
-#[no_mangle]
-#[inline(never)]
-pub extern "C" fn _main() -> ! {
-	let _ = main();
-	exit(ExitStatus::Success);
-}
+use fuste::println;
 
 pub fn add(a: u32, b: u32) -> u32 {
 	a + b
 }
 
-fn main() -> Result<(), ()> {
-	let mut j = 0;
-	for i in 0..10 {
-		assert_eq!(i, i);
-		j += i;
-		j = add(j, i);
-		println!("Hello, world!");
-		println!("i: {}, j: {}", i, j);
+fuste::entry! {
+	fn main() -> Result<(), ()> {
+		let mut j = 0;
+		for i in 0..10 {
+			assert_eq!(i, i);
+			j += i;
+			j = add(j, i);
+			println!("Hello, world!");
+			println!("i: {}, j: {}", i, j);
+		}
+
+		Ok(())
 	}
-
-	Ok(())
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-	exit(ExitStatus::Error);
 }
