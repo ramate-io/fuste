6dad56c98d325eb0801ac42154c771cc07384f49
Liam Makena Monninger <liam@ramate.io>
2025-11-02 16:35:41 +0100

    chore: tick handler.

--- fuste/systems/tick/handler/src/lib.rs
+++ fuste/systems/tick/handler/src/lib.rs
@@ -1,81 +1,31 @@
 #![no_std]
 
 use core::ops::ControlFlow;
-use fuste_riscv_core::{
-	instructions::{EbreakInterrupt, EcallInterrupt, ExecutableInstructionError},
-	machine::{Machine, MachineError, MachineSystem},
-};
+use fuste_riscv_core::machine::{Machine, MachineError, MachineSystem};
 
-/// The [EcallHandlerOps] trait provides the operations for handling ecall interrupts.
-///
-/// Specifically, it allows setting the ecall interrupt before ticking the plugin.
-pub trait EcallHandlerOps<const MEMORY_SIZE: usize>: MachineSystem<MEMORY_SIZE> {
-	fn set_ecall_interrupt(&mut self, interrupt: EcallInterrupt) -> Result<(), MachineError>;
-
-	fn tick_with_ecall_interrupt(
-		&mut self,
-		machine: &mut Machine<MEMORY_SIZE>,
-		ecall_interrupt: EcallInterrupt,
-	) -> Result<ControlFlow<()>, MachineError> {
-		self.set_ecall_interrupt(ecall_interrupt)?;
-		self.tick(machine)
-	}
-}
-
-/// The [EbreakHandlerOps] trait provides the operations for handling ebreak interrupts.
-///
-/// Specifically, it allows setting the ebreak interrupt before ticking the plugin.
-pub trait EbreakHandlerOps<const MEMORY_SIZE: usize>: MachineSystem<MEMORY_SIZE> {
-	fn set_ebreak_interrupt(&mut self, interrupt: EbreakInterrupt) -> Result<(), MachineError>;
-
-	fn tick_with_ebreak_interrupt(
-		&mut self,
-		machine: &mut Machine<MEMORY_SIZE>,
-		ebreak_interrupt: EbreakInterrupt,
-	) -> Result<ControlFlow<()>, MachineError> {
-		self.set_ebreak_interrupt(ebreak_interrupt)?;
-		self.tick(machine)
-	}
-}
-
-/// The [InterruptHandler] plugin handles interrupts ticking and inner machine then delegating to the appropriate handler.
-pub struct InterruptHandler<
-	const MEMORY_SIZE: usize,
-	Inner: MachineSystem<MEMORY_SIZE>,
-	EcallHandler: MachineSystem<MEMORY_SIZE>,
-	EbreakHandler: EbreakHandlerOps<MEMORY_SIZE>,
-> {
+/// The [TickHandler] plugin handles ticking the inner machine.
+pub struct TickHandler<const MEMORY_SIZE: usize, Inner: MachineSystem<MEMORY_SIZE>> {
 	pub inner: Inner,
-	pub ecall_handler: EcallHandler,
-	pub ebreak_handler: EbreakHandler,
+	pub current_tick: u32,
+	pub max_ticks: u32,
 }
 
-impl<
-		const MEMORY_SIZE: usize,
-		Inner: MachineSystem<MEMORY_SIZE>,
-		EcallHandler: EcallHandlerOps<MEMORY_SIZE>,
-		EbreakHandler: EbreakHandlerOps<MEMORY_SIZE>,
-	> MachineSystem<MEMORY_SIZE>
-	for InterruptHandler<MEMORY_SIZE, Inner, EcallHandler, EbreakHandler>
+impl<const MEMORY_SIZE: usize, Inner: MachineSystem<MEMORY_SIZE>> MachineSystem<MEMORY_SIZE>
+	for TickHandler<MEMORY_SIZE, Inner>
 {
-	/// Ticks the interrupt handler and delegates to the appropriate handler based on the interrupt type.
-	///
-	/// We inline because this compositional pattern will most often be used to form a call for every loop.
-	/// Thus, this plugin will be called often.
+	/// Ticks up to the max ticks or until the inner machine returns a break.
 	#[inline(always)]
 	fn tick(
 		&mut self,
 		machine: &mut Machine<MEMORY_SIZE>,
 	) -> Result<ControlFlow<()>, MachineError> {
-		match self.inner.tick(machine) {
-			Ok(control_flow) => Ok(control_flow),
-			Err(MachineError::InstructionError(ExecutableInstructionError::EcallInterrupt(
-				ecall_interrupt,
-			))) => self.ecall_handler.tick_with_ecall_interrupt(machine, ecall_interrupt),
-			Err(MachineError::InstructionError(ExecutableInstructionError::EbreakInterrupt(
-				ebreak_interrupt,
-			))) => self.ebreak_handler.tick_with_ebreak_interrupt(machine, ebreak_interrupt),
-			Err(e) => Err(e),
+		let result = self.inner.tick(machine)?;
+
+		self.current_tick += 1;
+		if self.current_tick >= self.max_ticks {
+			return Ok(ControlFlow::Break(()));
+		} else {
+			return Ok(result);
 		}
 	}
 }
