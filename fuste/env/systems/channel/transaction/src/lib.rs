5ef98a7defa22abee0fa6898238a34690bfea11d
Liam Makena Monninger <liam@ramate.io>
2026-01-07 15:56:21 -0800

    chore: transaction response.

--- fuste/env/systems/channel/transaction/src/lib.rs
+++ fuste/env/systems/channel/transaction/src/lib.rs
@@ -5,23 +5,23 @@ use fuste_serial_channel::{Deserialize, SerialChannelError, SerialType, Serializ
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct TransactionSchemeId(u32);
 
-pub trait TransactionScheme: SerialType {
+pub trait TransactionRequest: SerialType {
 	fn scheme_id() -> TransactionSchemeId;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-pub struct TransactionMetadataRequest<R: TransactionScheme> {
+pub struct TransactionMetadataRequest<R: TransactionRequest> {
 	pub scheme_id: TransactionSchemeId,
 	pub request: R,
 }
 
-impl<R: TransactionScheme> TransactionMetadataRequest<R> {
+impl<R: TransactionRequest> TransactionMetadataRequest<R> {
 	pub fn new(request: R) -> Self {
 		Self { scheme_id: R::scheme_id(), request }
 	}
 }
 
-impl<R: TransactionScheme> Serialize for TransactionMetadataRequest<R> {
+impl<R: TransactionRequest> Serialize for TransactionMetadataRequest<R> {
 	fn try_to_bytes<const N: usize>(&self) -> Result<(usize, [u8; N]), SerialChannelError> {
 		let scheme_id_bytes: [u8; 4] = self.scheme_id.0.to_le_bytes();
 		let (request_len, request_bytes) = self.request.try_to_bytes::<N>()?;
@@ -43,7 +43,7 @@ impl<R: TransactionScheme> Serialize for TransactionMetadataRequest<R> {
 	}
 }
 
-impl<R: TransactionScheme> Deserialize for TransactionMetadataRequest<R> {
+impl<R: TransactionRequest> Deserialize for TransactionMetadataRequest<R> {
 	fn try_from_bytes(bytes: &[u8]) -> Result<Self, SerialChannelError> {
 		// First 4 bytes should be the scheme id.
 		if bytes.len() < 4 {
@@ -60,4 +60,17 @@ impl<R: TransactionScheme> Deserialize for TransactionMetadataRequest<R> {
 
 		Ok(Self { scheme_id, request })
 	}
+}
+
+pub trait TransactionResponse<R: TransactionRequest, S, I>: SerialType {
+	/// Returns the scheme id for the transaction.
+	fn scheme_id() -> TransactionSchemeId {
+		R::scheme_id()
+	}
+
+	/// Returns the signers for the transaction up to a maximum of N signers.
+	fn signers<const N: usize>() -> [Option<S>; N];
+
+	/// Returns the id for the transaction.
+	fn id() -> I;
 }
